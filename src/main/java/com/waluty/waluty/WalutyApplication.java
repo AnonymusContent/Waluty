d986bd411e8642958fb66a935ed9d328b46c8102
Kuba <jakub.bielawski@wawasoft.com>
2018-04-03 22:50:06 +0200

    dodanie branchow

--- src/main/java/com/waluty/waluty/WalutyApplication.java
+++ src/main/java/com/waluty/waluty/WalutyApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WalutyApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(WalutyApplication.class, args);
 	}
